61492c1e95a1d300c0a77e9336d7213c37b0486f
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-09-15 00:24:57 +0400

    cart component added and card designed modified

---
+++ src/Component/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div>
+            <h3>Credit Hour Remaining</h3>
+        </div>
+    );
+};
+
+export default Cart;
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable react/jsx-key */
 import { useEffect, useState } from 'react';
 import './Home.css'
+import Cart from '../Cart/Cart';
 
 const Home = () => {
     const [allData, setAllData] = useState([]);
@@ -13,36 +15,25 @@ const Home = () => {
         <>
         <h1 className="course-title">Course Registration</h1> 
         <div className="body">
-           <div>
-           {/* <div className="card-body">
-            <img className='card-img' src="https://i.ibb.co/LCdRhBW/Rectangle-2-6.png" alt="" />
-            <h3>Introduction to C Programming</h3>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-            <div className='card-info'>
-                <p>Price : 25000</p>
-                <p>Credit : 2hr</p>
-            </div>
-            <button className='card-btn' type="button">Select</button>
-            </div> */}
-            {
-                allData.map((datum=>{
-                    <div className="card-body">
-            <img className='card-img' src="https://i.ibb.co/LCdRhBW/Rectangle-2-6.png" alt="" />
-            <h3>Introduction to C Programming</h3>
-            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
-            <div className='card-info'>
-                <p>Price : 25000</p>
-                <p>Credit : 2hr</p>
-            </div>
-            <button className='card-btn' type="button">Select</button>
-            </div>
-                }))
-            }
+           <div className='card-container'>
+           {allData.map((card=>(
+             <div className="card-body">
+             <img className='card-img' src={card.image} alt="" />
+              <h3>{card.title}</h3>
+              <p>{card.description}</p>
+              <div className='card-info'>
+                  <p>Price : {card.price}</p>
+                  <p>Credit : {card.credit}</p>
+              </div>
+              <button className='card-btn' type="button">Select</button>
+              </div>
+           )))}
+           
            </div>
             {/* cart section */}
 
             <div className='cart'>
-                <h3>cart</h3>
+                <Cart></Cart>
             </div>
            
         </div>
