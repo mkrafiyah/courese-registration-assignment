adfd248e8ee65c55cb06c7261cbb420be4a88473
mkrafiyah <rafiyah.mahbub@gmail.com>
2023-09-15 02:21:30 +0400

    course title added to the cart

--- src/Component/Cart/Cart.jsx
+++ src/Component/Cart/Cart.jsx
@@ -1,9 +1,28 @@
+/* eslint-disable react/prop-types */
 import './Cart.css'
 
-const Cart = () => {
+const Cart = ({selectCourse}) => {
+    console.log(selectCourse)
     return (
         <div>
             <h3>Credit Hour Remaining</h3>
+            <hr />
+            <h2>Course Name</h2>
+            {
+                selectCourse.map((course =>(
+             <div key={course.id}>
+           
+            <li>{course.title}</li>
+            
+           
+             </div>
+                )))
+            }
+            <hr />
+             <p>Total Credit Hour:{}</p>
+            <hr />
+            <p>Total Price:</p>
+            
         </div>
     );
 };
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -5,19 +5,36 @@ import Cart from '../Cart/Cart';
 
 const Home = () => {
     const [allData, setAllData] = useState([]);
+    const [selectCourse, setSelectCourse] = useState([]);
     useEffect(()=>{
         fetch('./data.json')
         .then(res=> res.json())
         .then(data => setAllData(data))
     },[]);
+    const handleSelectCourse = (card) =>{
+        let creditHour = card.credit;
+        
+    
+        console.log(creditHour)
+        const isExists = selectCourse.find((item)=>item.id == card.id);
+        if(isExists){
+           return alert('already taken')
+        }else{
+            selectCourse.forEach((item) =>{
+                creditHour = parseInt(creditHour.slice(0, 1)) + item.credit
+            })
+            setSelectCourse([...selectCourse, card])
+        }
+        
+    }
     console.log(allData)
     return (
-        <>
+        <div className='body'>
         <h1 className="course-title">Course Registration</h1> 
-        <div className="body">
+        <div className="main-body">
            <div className='card-container'>
            {allData.map((card=>(
-             <div className="card-body">
+             <div key={card.id} className="card-body">
              <img className='card-img' src={card.image} alt="" />
               <h3>{card.title}</h3>
               <p>{card.description}</p>
@@ -25,7 +42,7 @@ const Home = () => {
                   <p>Price : {card.price}</p>
                   <p>Credit : {card.credit}</p>
               </div>
-              <button className='card-btn' type="button">Select</button>
+              <button onClick={()=> handleSelectCourse(card)} className='card-btn' type="button">Select</button>
               </div>
            )))}
            
@@ -33,11 +50,11 @@ const Home = () => {
             {/* cart section */}
 
             <div className='cart'>
-                <Cart></Cart>
+                <Cart selectCourse={selectCourse}></Cart>
             </div>
            
         </div>
-        </>
+        </div>
     );
 };
 
